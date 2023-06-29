a9e5c848f010f651db0fba9bc85cb052255211cf
marwan-rouissi <115158061+marwan-rouissi@users.noreply.github.com>
2023-06-29 08:49:58 +0200

    update main file
    add relevant comments and the interraction with user

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from solvers.bruteforce import *
+from solvers.backtracking import *
 
 class Game():
     
@@ -6,7 +7,6 @@ class Game():
     to convert it into a (str) matrix"""
     def __init__(self, sudoku:str) -> None:
         self.sudoku = sudoku
-
         data = open(self.sudoku, "r")
         self.sudoku = []
         self.numSudoku = []
@@ -19,7 +19,7 @@ class Game():
         for r in self.sudoku:
             if len(r) >9:
                 r.pop()
-        
+ 
         self.convertToNumeric()
         self.s = ''.join(map(str,[''.join(map(str, i)) for i in self.sudoku]))
 
@@ -41,7 +41,7 @@ class Game():
                 print("+-------+-------+-------+")
             for c in range(len(puzzle[r])):
                 if c == 0 or c == 3 or c ==6:
-                    print("| ", end = "")
+                    print("|", end = " ")
                 if puzzle[r][c] != 0:
                     # print(f"\033[1;34m")
                     print(puzzle[r][c], end = " ")
@@ -62,7 +62,7 @@ class Game():
                 if c == 0 or c == 3 or c ==6:
                     print("| ", end = "")
 
-                if puzzle[r][c] != self.numSudoku[r][c]:
+                if puzzle[r][c] != int(self.sudoku[r][c]):
                     print(f"\033[1;32m{puzzle[r][c]}\033[0m", end = " ")
                 else:
                     print(puzzle[r][c], end = " ")
@@ -72,20 +72,27 @@ class Game():
     
     "method to run the brute force method from Bruteforce class"
     def run_bruteforce(self):
-        print("--------------")
-        print("PUZZLE TO SOLVE")
-        print("--------------")
-        game.draw(game.numSudoku)
-
         bruteForce = Bruteforce(self.s)
         bruteForce.sudoku_brute_force(game.s)
         print("--------------")
         print("SOLVED PUZZLE")
+        print("BRUTE FORCE")
         print("--------------")
         self.drawSolvedPuzzle(bruteForce.puzzleSolution)
         print(f"{bruteForce.combinaisons} possible combinaisons")
         print(f"found on {bruteForce.goodone}th attempt")
     
+    "method to run the back tracking method from Backtracking class"
+    def run_backtracking(self):
+        backtracking = Backtracking(game.numSudoku)
+        backtracking.sudoku_back_tracking()
+        print("--------------")
+        print("SOLVED PUZZLE")
+        print("BACK TRACKING")
+        print("--------------")
+        self.drawSolvedPuzzle(backtracking.puzzleSolution)
+        print(f"{backtracking.combinaisons} possible combinaisons")
+        print(f"found on {backtracking.goodone}th attempt")
 
 level_1 = "puzzles/sudoku.txt"
 level_2 = "puzzles/sudoku2.txt"
@@ -93,7 +100,33 @@ level_3 = "puzzles/sudoku3.txt"
 level_4 = "puzzles/sudoku4.txt"
 level_5 = "puzzles/evilsudoku.txt"
 
-game = Game(level_5)
+"""convert user's input into a variable to select the puzzle to solve"""
+def convertInput(s):
+    if s == "1":
+        s = level_1
+    if s == "2":
+        s = level_2
+    if s == "3":
+        s = level_3
+    if s == "4":
+        s = level_4
+    if s == "5":
+        s = level_5
+    return s
 
 if __name__ == "__main__":
-    game.run_bruteforce()
+    print("What level to solve ? (1-5)")
+    choiceLvl = input()
+    convertInput(choiceLvl)
+
+    game = Game(convertInput(choiceLvl))
+    print("--------------")
+    print("PUZZLE TO SOLVE")
+    print("--------------")
+    game.draw(game.numSudoku)
+    print("which method do you want to use ? (bruteforce / backtracking)")
+    methodChoice = input()
+    if methodChoice == "bruteforce":
+        game.run_bruteforce()
+    if methodChoice == "backtracking":
+        game.run_backtracking()
